cc71c8797c124410508b90952bc067f383c67eb1
Ritchie <wangzeyu@tom.com>
2024-06-19 15:40:20 +0800

    :tada: first commit

--- basic/read-file.py
+++ basic/read-file.py
@@ -1,11 +1,11 @@
 # ./data.txt前面的./可以省略
 # read() 返回全部文件内容的字符串
-f = open("txt/data.txt", encoding="utf-8")  # 默认是r读取模式
+f = open("../txt/data.txt", encoding="utf-8")  # 默认是r读取模式
 print(f.read())
 f.close()
 
 # readline() 返回一行内容的字符串
-f1 = open("txt/data1.txt", "r", encoding="utf-8")
+f1 = open("../txt/data1.txt", "r", encoding="utf-8")
 line = f1.readline()
 while line != "":
     print(line)
@@ -13,11 +13,11 @@ while line != "":
 f1.close()
 
 # readlines() 返回全部文件内容组成的列表
-f2 = open("txt/data2.txt", "r", encoding="utf-8")
+f2 = open("../txt/data2.txt", "r", encoding="utf-8")
 lines = f2.readlines()
 f2.close()
 for line in lines:
     print(line)
 
-with open("txt/data.txt", encoding="utf-8") as f:
+with open("../txt/data.txt", encoding="utf-8") as f:
     print(f.read())
--- basic/write-file.py
+++ basic/write-file.py
@@ -3,19 +3,19 @@
 # r+ 同时支持读写，覆盖
 # a+ 同时支持读写，追加
 
-with open("txt/write1.txt", "w", encoding="utf-8") as f:
+with open("../txt/write1.txt", "w", encoding="utf-8") as f:
     f.write("朝辞白帝彩云间\n")
     f.write("千里江陵一日还\n")
 
-with open("txt/write1.txt", "a", encoding="utf-8") as f:
+with open("../txt/write1.txt", "a", encoding="utf-8") as f:
     f.write("两岸猿声啼不住\n")
 
-with open("txt/write1.txt", "r+", encoding="utf-8") as f:
+with open("../txt/write1.txt", "r+", encoding="utf-8") as f:
     f.write("轻舟已过万重山\n")  # r+，会覆盖掉第一句
     # 此时文件指针在第一行末尾
     print(f.read())
 
-with open("txt/write1.txt", "a+", encoding="utf-8") as f:
+with open("../txt/write1.txt", "a+", encoding="utf-8") as f:
     f.write("轻舟已过万重山\n")  # a+ ，不会覆盖掉第一句
     # 此时文件指针在文件末尾
     f.seek(0)  # 将文件指针移动到文件开头
